4516506544344840814f89b316ef3f7eb406370b
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-10 18:46:55 +0100

    update

--- Pages/MainPage.razor.cs
+++ Pages/MainPage.razor.cs
@@ -5,6 +5,8 @@ namespace BlazorServer.Pages
 {
     public partial class MainPage
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         
         private async Task ClickEventHandler()
         {
@@ -13,6 +15,10 @@ namespace BlazorServer.Pages
         private async Task InputString(ChangeEventArgs input)
         {
             Console.WriteLine(input.Value);
+        }
+        private async Task NavigateToDetails()
+        {
+
         }
     }
 }
