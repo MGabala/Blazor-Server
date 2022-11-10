4a9cf5132d78436a44daa12aababbfe29968b210
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-10 14:41:38 +0100

    Add project files.

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BlazorServer.Pages
+{
+    public partial class Counter
+    {
+
+
+        private int currentCount = 0;
+        private async Task IncrementCount()
+        {
+            currentCount++;
+        }
+
+    }
+}
+ 
---
+++ Pages/MainPage.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServer.Pages
+{
+    public partial class MainPage
+    {
+        public List<string>? Lista = new List<string>()
+        {
+            "First record",
+            "Second record"
+        };
+    }
+}
