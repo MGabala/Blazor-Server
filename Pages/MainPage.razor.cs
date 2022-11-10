e4e5357b375655db8d6547e61f8b3b5546833728
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-10 16:23:49 +0100

    update

--- Pages/MainPage.razor.cs
+++ Pages/MainPage.razor.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorServer.Pages
 {
     public partial class MainPage
     {
         
-        private async Task DoIt()
+        private async Task ClickEventHandler()
         {
 
+        }
+        private async Task InputString(ChangeEventArgs input)
+        {
+            Console.WriteLine(input.Value);
         }
     }
 }
