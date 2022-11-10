b765627940d9eab37ad15b96415b5ef5ddb991e0
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-10 18:51:00 +0100

    update

--- Pages/MainPage.razor.cs
+++ Pages/MainPage.razor.cs
@@ -5,20 +5,28 @@ namespace BlazorServer.Pages
 {
     public partial class MainPage
     {
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-        
+
+        #region SimplyHitTheBreakPoint
         private async Task ClickEventHandler()
         {
 
         }
+        #endregion
+        #region InputPassingToMethod
         private async Task InputString(ChangeEventArgs input)
         {
             Console.WriteLine(input.Value);
         }
+        #endregion
+        #region HowToHitBreakPointViaButton
+        //This is how you can hit methods via code from page
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         private async Task NavigateToDetails()
         {
 
         }
+        //This is how you can hit methods via code from page
+        #endregion
     }
 }
